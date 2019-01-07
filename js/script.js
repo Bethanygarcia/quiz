afb39b555bb947f2d79bffa385572d28888a06ed
Bethany Garcia <bethanygarcia817@gmail.com>
2019-01-07 23:12:41 +0000

    game of thrones

--- js/script.js
+++ js/script.js
@@ -3,12 +3,17 @@
 $(document).ready(function() {
     $("button").click(function() {
         var name = $("#name").val();
+            $("#name").html("Hello" + name + "!");
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
+        
+        var army = $("input[name='command']:checked").val();
     });
 
+    
+    
 });
